8a34e35c6c9f4c631125911ab09b64637ee55592
loreletins <riita.lyn@gmail.com>
2017-08-23 19:53:48 -0300

    agregando movimiento de mouse

--- dibujarConTeclas/js/eventos.js
+++ dibujarConTeclas/js/eventos.js
@@ -5,7 +5,39 @@ var teclas = {
 	RIGHT: 39
 };
 
-console.log(teclas);
+var estado = 0;
+var colorsete = "blue";
+var cuadrito = document.getElementById("area_de_dibujo");
+var papel = cuadrito.getContext("2d");
+var x;
+var y;
+
+
+document.addEventListener("mousedown", presionarMouse);
+
+document.addEventListener("mouseup", soltarMouse);
+
+document.addEventListener("mousemove", moverMouse);
+
+function moverMouse(evento){
+  if (estado == 1) { 
+    dibujarLinea(colorsete, x, y, evento.layerX, evento.layerY, papel);
+  }
+  x = evento.layerX;
+  y = evento.layerY;
+}
+
+function presionarMouse(evento){
+  estado = 1;        
+  x = evento.layerX;
+  y = evento.layerY;
+}
+
+function soltarMouse(evento){
+  estado = 0;         // click suelto
+  x = evento.layerX;
+  y = evento.layerY;
+}
 
 document.addEventListener("keydown", dibujarTeclado);
 var cuadrito = document.getElementById("area_de_dibujo");
