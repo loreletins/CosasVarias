8f564951c3c5117648c362ea61dd9d188a4ace50
ljara <ljara@msa.com.ar>
2018-05-18 18:49:28 -0300

    la serpiente crece, suma puntos y tiene limites donde pierdes

--- Snake/snake.py
+++ Snake/snake.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+import time
+
 Blanco = (255, 255, 255)
 Negro = (0, 0, 0)
 Rojo = (255, 0, 0)
@@ -14,11 +16,32 @@ mover_x = 300
 mover_y = 300
 mover_x_cambio = 0
 mover_y_cambio = 0
+serp_tamano = 10
+ancho = 800
+altura = 500
+cuadro = 15
+
+font = pygame.font.SysFont(None, 25)
+
+def serpiente (serp_tamano, listaSerpiente):
+	for i in listaSerpiente:
+		pygame.draw.rect(superficie, Negro, [i[0], i[1], serp_tamano, serp_tamano])
+
+def message_to_screen(msg, color):
+	pantalla_texto = font.render(msg, True, color)
+	superficie.blit(pantalla_texto, [300, 300])
+
+listaSerpiente = []
+largoSerpiente = 1
 
 azarManzanaX = round(random.randrange(0, 300 - 10)/10.0)*10.0
 azarManzanaY = round(random.randrange(0, 300 - 10)/10.0)*10.0
 
 reloj = pygame.time.Clock()
+
+def puntos(score):
+	text = font.render("Puntos: " + str(score), True, Negro)
+	superficie.blit(text, [0, 0])
 
 while not gameExit:
 	for event in pygame.event.get():
@@ -26,29 +49,47 @@ while not gameExit:
 			gameExit = True
 	if event.type == pygame.KEYDOWN:
 		if event.key == pygame.K_LEFT:
-			mover_x_cambio = -10
+			mover_x_cambio = - serp_tamano
 			mover_y_cambio = 0
 		elif event.key == pygame.K_RIGHT:
-			mover_x_cambio = 10
+			mover_x_cambio = serp_tamano
 			mover_y_cambio = 0
 		elif event.key == pygame.K_UP:
-			mover_y_cambio = -10
+			mover_y_cambio = - serp_tamano
 			mover_x_cambio = 0
 		elif event.key == pygame.K_DOWN:
-			mover_y_cambio = 10
+			mover_y_cambio = serp_tamano
 			mover_x_cambio = 0
+
+	if mover_x >= ancho or mover_x < 0 or mover_y >= altura or mover_y <0:
+		gameExit = True
 
 	mover_y += mover_y_cambio
 	mover_x += mover_x_cambio
 	superficie.fill(Blanco)
 	pygame.draw.rect(superficie, Rojo, [azarManzanaX, azarManzanaY, 10, 10])
-	pygame.draw.rect(superficie, Negro, [mover_x, mover_y, 10, 10])
+
+	cabezaSerpiente = []
+
+	cabezaSerpiente.append(mover_x)
+	cabezaSerpiente.append(mover_y)
+	listaSerpiente.append(cabezaSerpiente)
+	if len(listaSerpiente)> largoSerpiente:
+		del listaSerpiente[0]
+	serpiente(serp_tamano, listaSerpiente)
+	puntos(largoSerpiente-1)
 	pygame.display.update()
 
 	if mover_x == azarManzanaX and mover_y == azarManzanaY:
 		azarManzanaX = round(random.randrange(0, 300 - 10)/10.0)*10.0
 		azarManzanaY = round(random.randrange(0, 300 - 10)/10.0)*10.0
-	reloj.tick(15)
+		largoSerpiente += 1
+
+	reloj.tick(cuadro)
+
+message_to_screen("Has perdido", Rojo)
+pygame.display.update()
+time.sleep(3)
 
 pygame.quit()
 quit()
