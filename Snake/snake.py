a8e43d9cb133a537ba0f31ba5d6febcb6a799868
ljara <ljara@msa.com.ar>
2018-05-18 14:17:19 -0300

    agrego manzana

--- Snake/snake.py
+++ Snake/snake.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 Blanco = (255, 255, 255)
 Negro = (0, 0, 0)
+Rojo = (255, 0, 0)
 
 pygame.init()
 superficie = pygame.display.set_mode((800,500))
@@ -12,6 +14,10 @@ mover_x = 300
 mover_y = 300
 mover_x_cambio = 0
 mover_y_cambio = 0
+
+azarManzanaX = random.randrange(0, 800 -10)
+azarManzanaY = random.randrange(0, 400 -10)
+
 reloj = pygame.time.Clock()
 
 while not gameExit:
@@ -35,10 +41,11 @@ while not gameExit:
 	mover_y += mover_y_cambio
 	mover_x += mover_x_cambio
 	superficie.fill(Blanco)
+	pygame.draw.rect(superficie, Rojo, [azarManzanaX, azarManzanaY, 10, 10])
 	pygame.draw.rect(superficie, Negro, [mover_x, mover_y, 10, 10])
 	pygame.display.update()
 
-	reloj.tick(30)
+	reloj.tick(10)
 
 pygame.quit()
 quit()
